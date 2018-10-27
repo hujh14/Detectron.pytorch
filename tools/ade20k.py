76b63f9e3a1d87087a242cc9f6665e95e0af5195
Jeffrey Hu <hujh@visiongpu03.csail.mit.edu>
2018-10-27 19:46:33 -0400

    oops

--- tools/ade20k.py
+++ tools/ade20k.py
@@ -7,13 +7,13 @@ _DATA_DIR = cfg.DATA_DIR
 
 DATASETS["ade20k_train"] = {
     IM_DIR:
-        _DATA_DIR + '/ade20k/images/training',
+        _DATA_DIR + '/ade20k/images',
     ANN_FN:
         _DATA_DIR + '/ade20k/annotations/instances/ade20k_train_annotations.json',
 }
 DATASETS["ade20k_val"] = {
     IM_DIR:
-        _DATA_DIR + '/ade20k/images/validation',
+        _DATA_DIR + '/ade20k/images',
     ANN_FN:
         _DATA_DIR + '/ade20k/annotations/instances/ade20k_val_annotations.json',
 }
