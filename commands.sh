08f442b3b943e9c846418561859ab83cad87f34f
Jeffrey Hu <hujh@mit.edu>
2018-10-27 19:28:14 -0400

    path change

--- commands.sh
+++ commands.sh
@@ -2,3 +2,4 @@ CUDA_VISIBLE_DEVICES=0 python tools/test_net.py --dataset coco2017 --cfg configs
 
 CUDA_VISIBLE_DEVICES=0 python tools/infer_simple.py --dataset coco --cfg configs/baselines/e2e_mask_rcnn_R-50-FPN_1x.yaml --load_detectron data/detectron_model/mask_rcnn_R-50-FPN_1x.pkl --image_dir ../datasets/ade20k/images/validation
 
+python tools/train_net_step.py --dataset ade20k --cfg configs/baselines/e2e_mask_rcnn_R-50-FPN_1x.yaml --use_tfboard
--- tools/ade20k.py
+++ tools/ade20k.py
@@ -9,11 +9,11 @@ DATASETS["ade20k_train"] = {
     IM_DIR:
         _DATA_DIR + '/ade20k/images/training',
     ANN_FN:
-        _DATA_DIR + '/ade20k/annotations/annotations_instance/ade20k_train_annotations.json',
+        _DATA_DIR + '/ade20k/annotations/instances/ade20k_train_annotations.json',
 }
 DATASETS["ade20k_val"] = {
     IM_DIR:
         _DATA_DIR + '/ade20k/images/validation',
     ANN_FN:
-        _DATA_DIR + '/ade20k/annotations/annotations_instance/ade20k_val_annotations.json',
+        _DATA_DIR + '/ade20k/annotations/instances/ade20k_val_annotations.json',
 }
